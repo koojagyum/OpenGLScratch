0a2d6bc5f38cd1a87ab453687611a1fa2c09b1ae
Jagyum Koo <koojagyum@gmail.com>
2017-02-28 01:28:33 +0900

    Build basic UI

--- OpenGLScratch/ViewController.swift
+++ OpenGLScratch/ViewController.swift
@@ -8,12 +8,25 @@
 
 import Cocoa
 
+// Move this into the proper module
+enum SceneType: String {
+    case Triangle, Cube, Camera
+}
+
 class ViewController: NSViewController {
+
+    @IBOutlet weak var popUpButton: NSPopUpButtonCell!
+    @IBOutlet weak var infoLabel: NSTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        popUpButton.removeAllItems()
+        let availableScenes = [SceneType.Triangle, SceneType.Cube, SceneType.Camera]
+        for i in availableScenes {
+            popUpButton.addItem(withTitle: i.rawValue)
+        }
     }
 
     override var representedObject: Any? {
@@ -22,6 +35,9 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func runButton(_ sender: Any) {
+        infoLabel.stringValue = popUpButton.titleOfSelectedItem!
+    }
 
 }
 
