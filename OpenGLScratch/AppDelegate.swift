1e03f41e5693f9b18d6194e93083f33bd5d465a5
Jagyum Koo <koojagyum@gmail.com>
2017-02-27 23:59:42 +0900

    Initial commit

---
+++ OpenGLScratch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OpenGLScratch
+//
+//  Created by Jagyum Koo on 2017. 2. 27..
+//  Copyright © 2017년 Jagyum Koo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OpenGLScratch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OpenGLScratch
+//
+//  Created by Jagyum Koo on 2017. 2. 27..
+//  Copyright © 2017년 Jagyum Koo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
