9bc100260f4ad345f4a305536896bb7c8172f9f8
Jagyum Koo <koojagyum@gmail.com>
2017-03-09 01:11:08 +0900

    Add OpenGL context

---
+++ OpenGLScratch/MyOpenGLView.swift
@@ -0,0 +1,60 @@
+//
+//  MyOpenGLView.swift
+//  OpenGLScratch
+//
+//  Created by Jagyum Koo on 2017. 3. 8..
+//  Copyright © 2017년 Jagyum Koo. All rights reserved.
+//
+
+import Cocoa
+import OpenGL.GL
+
+protocol MyOpenGLRendererDelegate {
+    func prepare()
+    func render()
+    func dispose()
+}
+
+class MyOpenGLView : NSOpenGLView {
+    private var _renderer: MyOpenGLRendererDelegate?
+    var renderer: MyOpenGLRendererDelegate? {
+        get {
+            return self._renderer
+        }
+        set {
+            _renderer?.dispose()
+            _renderer = newValue
+            _renderer?.prepare()
+        }
+    }
+
+    override func draw(_ dirtyRect: NSRect) {
+        guard let context = self.openGLContext else {
+            return
+        }
+        NSLog("draw called\n")
+        context.makeCurrentContext()
+        self.renderer?.render()
+        context.flushBuffer()
+    }
+
+    override func awakeFromNib() {
+        let pixelFormatAttributes:[NSOpenGLPixelFormatAttribute] = [
+            NSOpenGLPixelFormatAttribute(NSOpenGLPFADoubleBuffer),
+            NSOpenGLPixelFormatAttribute(NSOpenGLPFAAccelerated), 0,
+            NSOpenGLPixelFormatAttribute(NSOpenGLPFAOpenGLProfile),
+            NSOpenGLPixelFormatAttribute(NSOpenGLProfileVersion4_1Core),
+            0
+        ]
+
+        guard let pixelFormat = NSOpenGLPixelFormat(attributes:pixelFormatAttributes) else {
+            fatalError("No appropriate pixel format found when creating OpenGL context.")
+        }
+        // TODO: Take into account the sharegroup
+        guard let generatedContext = NSOpenGLContext(format:pixelFormat, share:nil) else {
+            fatalError("Unable to create an OpenGL context. The GPUImage framework requires OpenGL support to work.")
+        }
+
+        self.openGLContext = generatedContext
+    }
+}
