9f453f807c09dedb85ad9c5da5cf75bd74bbfc63
Jagyum Koo <koodev@Jagyums-MacBook-Pro.local>
2017-05-29 21:33:27 +0900

    Add PointLightRenderer

--- OpenGLScratch/ViewController.swift
+++ OpenGLScratch/ViewController.swift
@@ -10,7 +10,7 @@ import Cocoa
 
 // Move these into the proper module
 enum SceneType: String {
-    case Triangle, Rectangle, TriangleTexture, RectangleTexture, RectangleRotation, RectanglePerspective, Cube, NCubes, NCubesWithCamera, LightingAndLamp, Circle, SpinningLamp, LightingWithMaterial, LightingWithMaps, DirectionalLight
+    case Triangle, Rectangle, TriangleTexture, RectangleTexture, RectangleRotation, RectanglePerspective, Cube, NCubes, NCubesWithCamera, LightingAndLamp, Circle, SpinningLamp, LightingWithMaterial, LightingWithMaps, DirectionalLight, PointLight
 }
 
 enum PolygonMode: String {
@@ -35,6 +35,7 @@ class ViewController: NSViewController {
         SceneType.LightingWithMaterial: LightingWithMaterialRenderer(),
         SceneType.LightingWithMaps: LightingWithMapsRenderer(),
         SceneType.DirectionalLight: DirectionalLightRenderer(),
+        SceneType.PointLight: PointLightRenderer(),
     ]
 
     override func viewDidLoad() {
